0842b7b2b37f7645c777b2f23727ee14f878d216
Nikhil <nikhil.speed@gmail.com>
2021-04-18 17:35:03 +0530

    beta 1

--- index.js
+++ index.js
@@ -12,21 +12,36 @@ app.use(express.static(path.join(__dirname, "/public")));
 let blogs = [
   {
     id: uuid(),
-    title: "Hazards of the Internet",
+    title: "Eating Healthy",
     image:
-      "https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      "https://images.unsplash.com/photo-1547592180-85f173990554?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
     author: "Nikhil",
-    content:
-      "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem  lorem lorem lorem lorem lorem lorem lorem lorem  ",
+    content: `A healthy diet helps pave the way to a healthy heart and blood vessels, strong bones and muscles, a sharp mind, and so much more.
+
+      Confused about what constitutes a healthy diet? You aren't alone. Over the years, what seemed to be flip flops from medical research combined with the flood of diet books and diet plans based on little or no science have muddied the water. But a consensus has emerged about the basics, which are really pretty simple.
+      
+      An important take-home message is to focus on the types of foods you eat and your overall dietary pattern, instead of on individual nutrients such as fat, dietary cholesterol, or specific vitamins. There are no single nutrients or vitamins that can make you healthy. Instead, there is a short list of key food types that together can dramatically reduce your risk for heart disease.
+      
+      Eat more of these foods: fruits and vegetables, whole grains, fish and seafood, vegetable oils, beans, nuts, and seeds.
+      
+      Eat less of these foods: whole milk and other full-fat dairy foods, red meat, processed meats, highly refined and processed grains and sugars, and sugary drinks.`,
   },
   {
     id: uuid(),
-    title: "Hazards of the Internet",
+    title: "Benefits of Oranges",
     image:
-      "https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      "https://images.unsplash.com/photo-1618502913824-e45c41e932d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
     author: "Nikhil",
-    content:
-      "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem  lorem lorem lorem lorem lorem lorem lorem lorem  ",
+    content: `Oranges are a round, segmented citrus fruit with a pitted peel. The taste can vary from juicy and sweet to bitter, depending on the variety – more common ones include Valencia, Seville and Hamlin. Most oranges are available year-round, except for varieties such as blood oranges, which have a shorter season.
+
+    Nutritional benefits of oranges
+    Oranges are known for their vitamin C content, a powerful antioxidant that helps protect cells from damage. One medium orange will provide the NRV (nutrient reference value) of vitamin C for adults.
+    
+    They also contain health-promoting compounds known as flavanones. Research suggests that these citrus phytochemicals help support the body and protect us from conditions such as heart disease and cancer – they’re also thought to have some anti-inflammatory, antiviral and antimicrobial benefits.
+    
+    Oranges are also a good source of fibre, B vitamins, vitamin A, calcium and potassium.
+    
+    One orange counts towards one of your five-a-day. A 150ml glass of unsweetened orange juice also counts as one portion, although the NHS advises that orange juice, as with other juices, can only count once per day no matter how much you drink. Orange juice doesn’t contain the fibre that is present in the whole fruit and is high in sugars, so the NHS advises that fruit juice is best consumed with food to limit the damage to teeth.`,
   },
 ];
 
